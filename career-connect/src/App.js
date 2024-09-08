d3156b74af54f10ced932b6ffa7ae4f4f6864657
Djordje <stefanovic.djordje1997@gmail.com>
2024-09-08 23:19:41 +0200

    Routes
    Create pages folder and add empty files for pages. Update App.js with routing setup.

--- career-connect/src/App.js
+++ career-connect/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import StudentDashboard from './pages/StudentDashboard.jsx';
+import CompanyDashboard from './pages/CompanyDashboard';
+import CreateJob from './pages/CreateJob';
+import ProfilePage from './pages/ProfilePage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/company-dashboard" element={<CompanyDashboard />} />
+        <Route path="/create-job" element={<CreateJob />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
   );
 }
 
